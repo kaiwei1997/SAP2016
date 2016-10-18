4f30bb8af4af57ba10c263c84ef2e51efc2622c3
Josh Tan <Josh Tan>
2016-10-19 02:03:35 +0800

    Initial commit

---
+++ app/src/main/java/com/sap/josh0207/sap2016/BrandHomepageActivity.java
@@ -0,0 +1,13 @@
+package com.sap.josh0207.sap2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrandHomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brand_homepage);
+    }
+}
