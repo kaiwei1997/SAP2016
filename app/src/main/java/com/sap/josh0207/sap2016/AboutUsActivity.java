f0af7d5a10d0aafb42b4afb9047afc8be0449546
Josh0207 <kaiweitan128@gmail.com>
2016-11-06 03:01:57 +0800

    N/A

--- app/src/main/java/com/sap/josh0207/sap2016/AboutUsActivity.java
+++ app/src/main/java/com/sap/josh0207/sap2016/AboutUsActivity.java
@@ -18,55 +18,9 @@ import com.theartofdev.edmodo.cropper.CropImageView;
 import java.util.Date;
 
 public class AboutUsActivity extends AppCompatActivity {
-    private Button test1;
-    private ImageView test2;
-
-    private static final int GALLERY_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-
-        test1 = (Button)findViewById(R.id.tester1);
-        test2 = (ImageView)findViewById(R.id.tester2);
-
-        test1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Test",Toast.LENGTH_LONG).show();
-                Intent galleryIntent = new Intent();
-                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent,GALLERY_REQUEST);
-            }
-        });
-    }
-
-    @Override
-    public void onActivityResult(int requestCode,int resultCode,Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
-
-            Uri imageUri = data.getData();
-
-            CropImage.activity(imageUri).setGuidelines(CropImageView.Guidelines.ON).start(this);
-        }
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-
-            if (resultCode == RESULT_OK) {
-
-                Uri resultUri = result.getUri();
-
-                test2.setImageURI(resultUri);
-
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-
-                Exception error = result.getError();
-                error.printStackTrace();
-            }
-        }
     }
 }
