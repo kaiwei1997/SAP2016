6519efb269c0fe4df9ed1d672584532844d2db2e
Josh0207 <kaiweitan128@gmail.com>
2016-11-06 14:38:59 +0800

    N/A

---
+++ app/src/main/java/com/sap/josh0207/sap2016/MerchantProfileSettingFragment.java
@@ -0,0 +1,23 @@
+package com.sap.josh0207.sap2016;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+public class MerchantProfileSettingFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_merchant_profile_setting, container, false);
+
+        return view;
+    }
+}
