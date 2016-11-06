6a1b8a293db8e767484cc055d4cd5520c0595bf9
Josh0207 <kaiweitan128@gmail.com>
2016-11-06 16:30:54 +0800

    N/A

--- app/src/main/java/com/sap/josh0207/sap2016/fragment/BrandCampaignFragment.java
+++ app/src/main/java/com/sap/josh0207/sap2016/fragment/BrandCampaignFragment.java
@@ -8,13 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.sap.josh0207.sap2016.R;
 
 public class BrandCampaignFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_brand_campaign, container, false);
+        View view = inflater.inflate(R.layout.fragment_brand_campaign, container, false);
+        //Load AdMob
+        MobileAds.initialize(getActivity(),"ca-app-pub-4516935382926964~7526464337");
+        AdView mAdView = (AdView) view.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        return view;
     }
 }
