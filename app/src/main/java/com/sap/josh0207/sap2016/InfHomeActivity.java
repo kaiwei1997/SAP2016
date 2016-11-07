e8d77eedcdfebf2fe552efe211e99ddf23cd1bb7
Josh0207 <kaiweitan128@gmail.com>
2016-11-07 23:41:42 +0800

    N/A

--- app/src/main/java/com/sap/josh0207/sap2016/InfHomeActivity.java
+++ app/src/main/java/com/sap/josh0207/sap2016/InfHomeActivity.java
@@ -69,7 +69,7 @@ public class InfHomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (DrawerLayout) findViewById(R.id.inf_drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
